37d2ff9ce7c79891b6b478ee886669b1b610ca4c
dawoonkim <hec8897@naver.com>
2026-01-07 16:18:49 +0900

    chore: 프로젝트 초기 설정 및 RecordList 컴포넌트 생성 - 기본 템플릿 코드 제거 - CSS 리셋
    Signed-off-by: dawoonkim <hec8897@naver.com>

---
+++ src/domains/record/components/RecordList/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const RecordList = () => {
+  return <div>RecordList</div>;
+};
--- src/domains/record/index.tsx
+++ src/domains/record/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import { RecordList } from './components/RecordList';
 
 export const Record = () => {
-  return <div>Record</div>;
+  return <RecordList />;
 };
