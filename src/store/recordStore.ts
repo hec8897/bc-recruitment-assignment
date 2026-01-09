e59416b426634ff0474c2a82d67b4981a833e015
dawoonkim <hec8897@naver.com>
2026-01-10 01:03:02 +0900

    feat: Zustand persist middleware 적용
    Signed-off-by: dawoonkim <hec8897@naver.com>

--- src/store/recordStore.ts
+++ src/store/recordStore.ts
@@ -1,5 +1,12 @@
-import { create } from 'zustand';
+import {
+  create,
+  type StateCreator,
+} from 'zustand';
 import { v4 as uuidv4 } from 'uuid';
+import {
+  persist,
+  createJSONStorage,
+} from 'zustand/middleware';
 
 import type {
   FieldID,
@@ -11,84 +18,97 @@ import { mockRecords } from './mock';
 
 import type { RecordState } from './recordStore.type';
 
-export const useRecordStore = create<RecordState>(
-  (set, get) => ({
-    records: mockRecords,
-    filters: {
-      name: [],
-      address: [],
-      memo: [],
-      joinDate: [],
-      job: [],
-      emailAgree: [],
-    },
-    getUniqueValues: (field: FieldID) => {
-      const records = get().records;
-      return Array.from(
-        new Set(records.map((r) => r[field]))
-      );
-    },
-    setFilter: (
-      field: FieldID,
-      values: Record[FieldID][]
-    ) => {
-      set((state) => ({
-        filters: {
-          ...state.filters,
-          [field]: values,
-        },
-      }));
-    },
-    getFilteredRecords: () => {
-      const { records, filters } = get();
-      return records.filter((record) => {
-        return (
-          Object.keys(filters) as FieldID[]
-        ).every((field) => {
-          return isMatchFilter(
-            field,
-            record[field],
-            filters[field]
-          );
-        });
-      });
-    },
-    addRecord: (
-      newRecord: Omit<Record, 'id'>
-    ) => {
-      const id = uuidv4();
+const storageType =
+  import.meta.env.VITE_STORAGE || 'in-memory';
 
-      set((state) => ({
-        records: [
-          ...state.records,
-          { ...newRecord, id },
-        ],
-      }));
-    },
-    getRecordById: (id: string) => {
-      const { records } = get();
-      return records.find(
-        (record) => record.id === id
-      );
-    },
-    updateRecord: (
-      id: string,
-      updatedData: Omit<Record, 'id'>
-    ) => {
-      set((state) => ({
-        records: state.records.map((record) =>
-          record.id === id
-            ? { ...record, ...updatedData }
-            : record
-        ),
-      }));
-    },
-    deleteRecord: (id: string) => {
-      set((state) => ({
-        records: state.records.filter(
-          (record) => record.id !== id
-        ),
-      }));
-    },
-  })
-);
+const createRecordStore: StateCreator<
+  RecordState
+> = (set, get) => ({
+  records: mockRecords,
+  filters: {
+    name: [],
+    address: [],
+    memo: [],
+    joinDate: [],
+    job: [],
+    emailAgree: [],
+  },
+  getUniqueValues: (field: FieldID) => {
+    const records = get().records;
+    return Array.from(
+      new Set(records.map((r) => r[field]))
+    );
+  },
+  setFilter: (
+    field: FieldID,
+    values: Record[FieldID][]
+  ) => {
+    set((state) => ({
+      filters: {
+        ...state.filters,
+        [field]: values,
+      },
+    }));
+  },
+  getFilteredRecords: () => {
+    const { records, filters } = get();
+    return records.filter((record) => {
+      return (
+        Object.keys(filters) as FieldID[]
+      ).every((field) => {
+        return isMatchFilter(
+          field,
+          record[field],
+          filters[field]
+        );
+      });
+    });
+  },
+  addRecord: (newRecord: Omit<Record, 'id'>) => {
+    const id = uuidv4();
+
+    set((state) => ({
+      records: [
+        ...state.records,
+        { ...newRecord, id },
+      ],
+    }));
+  },
+  getRecordById: (id: string) => {
+    const { records } = get();
+    return records.find(
+      (record) => record.id === id
+    );
+  },
+  updateRecord: (
+    id: string,
+    updatedData: Omit<Record, 'id'>
+  ) => {
+    set((state) => ({
+      records: state.records.map((record) =>
+        record.id === id
+          ? { ...record, ...updatedData }
+          : record
+      ),
+    }));
+  },
+  deleteRecord: (id: string) => {
+    set((state) => ({
+      records: state.records.filter(
+        (record) => record.id !== id
+      ),
+    }));
+  },
+});
+
+export const useRecordStore =
+  storageType === 'local-storage'
+    ? create<RecordState>()(
+        persist(createRecordStore, {
+          name: 'record-storage',
+          storage: createJSONStorage(
+            () => localStorage
+          ),
+        })
+      )
+    : create<RecordState>()(createRecordStore);
